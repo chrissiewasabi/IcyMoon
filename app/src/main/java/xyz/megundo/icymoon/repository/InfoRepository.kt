21fe27d8946f29f79ebf22435d7fb728910cf7a3
christinah <christinewasabi@gmail.com>
2019-08-28 07:15:02 +0300

    add error handling

--- app/src/main/java/xyz/megundo/icymoon/repository/InfoRepository.kt
+++ app/src/main/java/xyz/megundo/icymoon/repository/InfoRepository.kt
@@ -5,19 +5,24 @@ import androidx.lifecycle.MutableLiveData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import xyz.megundo.icymoon.api.ApiResponse
 import xyz.megundo.icymoon.api.InfoApiService
 import xyz.megundo.icymoon.data.Information
 
 class InfoRepository(val apiService: InfoApiService) {
-    fun postData(): LiveData<Information> {
-        val data = MutableLiveData<Information>()
+    fun postData(): LiveData<ApiResponse> {
+        val data = MutableLiveData<ApiResponse>()
         val call = apiService.getResponse()
         call.enqueue(object : Callback<Information> {
             override fun onFailure(call: Call<Information>?, t: Throwable?) {
-            }
+                data.postValue(t?.let { ApiResponse(it) })
 
+            }
             override fun onResponse(call: Call<Information>, response: Response<Information>) {
-                data.postValue(response.body())
+                if (response.body() != null) {
+                    data.postValue(ApiResponse(response.body()!!))
+                }
+
             }
 
         })
