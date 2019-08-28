fa4a4485b21671515640e6c882eb3c775ec56222
christinah <christinewasabi@gmail.com>
2019-08-28 07:15:50 +0300

    change LiveData type

--- app/src/main/java/xyz/megundo/icymoon/ui/MainViewModel.kt
+++ app/src/main/java/xyz/megundo/icymoon/ui/MainViewModel.kt
@@ -1,17 +1,23 @@
 package xyz.megundo.icymoon.ui
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import xyz.megundo.icymoon.api.ApiResponse
 import xyz.megundo.icymoon.data.Information
 import xyz.megundo.icymoon.repository.RepositoryProvider
 
 
 class MainViewModel : ViewModel() {
-    val infoLiveData = MutableLiveData<Information>()
+    val infoLiveData = MediatorLiveData<ApiResponse>()
 
-    fun getResponseFromServer(data: Information): MutableLiveData<Information> {
+    fun getResponseFromServer(data: Information): MutableLiveData<ApiResponse> {
         val repository = RepositoryProvider.provideRepository()
-        infoLiveData.postValue(repository.postData().value)
+        infoLiveData.addSource(
+            repository.postData()
+        ) { apiResponse -> infoLiveData.setValue(apiResponse) }
+
+
         return infoLiveData
     }
 }
