0d3c04d1e3470b41e21460baead2fc03ec138d0f
christinah <christinewasabi@gmail.com>
2019-08-28 06:40:07 +0300

    show fragment

--- app/src/main/java/xyz/megundo/icymoon/MainActivity.kt
+++ app/src/main/java/xyz/megundo/icymoon/MainActivity.kt
@@ -1,7 +1,8 @@
 package xyz.megundo.icymoon
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import xyz.megundo.icymoon.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         Thread.sleep(3000)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 }
