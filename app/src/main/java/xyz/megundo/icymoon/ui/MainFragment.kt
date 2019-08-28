9fc28b45d89424f5eb8b7e26645dedb920c5b245
christinah <christinewasabi@gmail.com>
2019-08-28 06:39:30 +0300

    submit data on button click

---
+++ app/src/main/java/xyz/megundo/icymoon/ui/MainFragment.kt
@@ -0,0 +1,66 @@
+package xyz.megundo.icymoon.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.main_fragment.*
+import xyz.megundo.icymoon.R
+import xyz.megundo.icymoon.data.Application
+import xyz.megundo.icymoon.data.Information
+
+class MainFragment : Fragment() {
+    private val TAG = "MainFragment"
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        btn_submit.setOnClickListener {
+            val payload = getDataFromViews()
+            Log.d(TAG, "data $payload")
+            viewModel.getResponseFromServer(payload)
+        }
+    }
+
+    private fun getDataFromViews(): Information {
+        val fName = txtFname.text.toString()
+        val lName = txtLname.text.toString()
+        val phone = txtPhone.text.toString()
+        val email = txtEmail.text.toString()
+        val idNum = txtId.text.toString()
+        val description = txtDesc.text.toString()
+        val amount = ((txtAmount.text.toString())).toDouble()
+
+        val data = Application(
+            email = email,
+            first_name = fName,
+            id_number = idNum,
+            item_description = description,
+            item_value = amount,
+            last_name = lName,
+            phone_number = phone
+        )
+
+        return Information(data)
+
+
+    }
+
+
+}
